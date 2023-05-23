e473f2fbf1927873f273e4353e952e7f90e2494b
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-05-23 16:12:50 -0500

    added -t flag

--- gammastep_toggle.sh
+++ gammastep_toggle.sh
@@ -2,9 +2,16 @@
 
 TOGGLE=$HOME/.gammastep_toggle
 
+while getopts t: flag
+do
+    case "${flag}" in
+        t) temp=${OPTARG};;
+    esac
+done
+
 if [ ! -e $TOGGLE ]; then
     touch $TOGGLE
-    gammastep -O 2600K
+    gammastep -O $temp K 
 else
     rm $TOGGLE
     pkill gammastep 
